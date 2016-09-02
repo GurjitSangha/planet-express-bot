7197511c124d78adcb460f3ec5511aa90b826809
Gurjit Sangha <gurjit.sangha@sky.uk>
2016-09-02 14:24:26 +0100

    Got mvp of planetexpress bot

---
+++ index.js
@@ -0,0 +1,38 @@
+var Botkit = require('botkit');
+var controller = Botkit.slackbot();
+var bot = controller.spawn({
+  token: 'xoxb-75734614263-9IJdYYiOlFkiGAdWjpMcpaMs'
+})
+bot.startRTM(function(err,bot,payload) {
+  if (err) {
+    throw new Error('Could not connect to Slack');
+  }
+
+  var counts = {
+    'jw1540': 0,
+    'Geit': 0,
+    'MarkSG93': 0,
+    'GurjitSangha': 0
+  };
+
+  controller.hears(['Failed:'], ["direct_message","direct_mention","mention","ambient"], function(bot, message) {
+    var split = message.text.split(' ');
+    var user = split[2].split("'")[0];
+    counts[user]++;
+
+    if (counts[user] < 3)
+      bot.reply(message, 'Strike ' + counts[user] + ' for ' + user + '!');
+    else {
+      bot.reply(message, 'Strike ' + counts[user] + ' for ' + user + '! You\'re out! :p45:');
+      counts[user] = 0;
+    }
+  });
+
+  controller.hears(['Fixed:'], ["direct_message","direct_mention","mention","ambient"], function(bot, message) {
+    var split = message.text.split(' ');
+    var user = split[2].split("'")[0];
+    counts[user] = 0;
+
+    bot.reply(message, 'Well done ' + user + '! Your strikes have been reset');
+  });
+});
