7089e18952b60ec91005d2d7ba7dcdbd5eb45131
Gurjit Sangha <gurjit.sangha@sky.uk>
2016-12-19 13:13:52 +0000

    added olcb and parrot

--- src/bot.js
+++ src/bot.js
@@ -14,7 +14,21 @@ bot.startRTM(function(err, bot, payload) {
     // remove 'echo' and join the remaining elements
     split.shift();
     bot.reply(message, split.join(' '));
-  })
+  });
+
+  controller.hears(['olcb'], ['ambient'], function(bot, message) {
+    bot.reply(message, 'Hey, that\'s me!');
+  });
+
+  controller.hears(['parrot'], ['ambient', 'direct_mention'], function(bot, message) {
+    var parrots = [':parrot:', ':parrot-aussie:', ':parrot-christmas:', 'parrot-deal-with-it', ':parrot-middle:', ':bored_parrot:', ':chill_parrot:',
+      ':parrot-right:', ':parrot-slow:', ':parrotcop:', ':fast_parrot', ':icecream_parrot:', ':confusedparrot:', ':explodingparrot:',
+      ':fiestaparrot:', ':reversecongaparrot:', ':sadparrot:'];
+
+    var parrot = parrots[Math.floor(Math.random() * parrots.length)];
+
+    bot.reply(message, parrot);
+  });
 })
 
 module.exports = bot;
